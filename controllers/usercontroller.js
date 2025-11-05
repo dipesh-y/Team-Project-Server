cc44805979180bcf408accc2dba9289f9bef35cc
VINEET KUMAR RAI <vineetrai099@gmail.com>
2025-11-05 09:12:06 +0530

    L-30 Completed

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,9 +1,11 @@
 import UserModel from '../models/usermodel.js'
-import  bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken'
-import {sendEmail} from '../config/emailService.js';
+import  bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import sendEmailFun from '../config/sendEmail.js';
 import VerificationEmail from './../utils/verifyEmailTemplate.js';
+import generatedAccessToken from '../utils/generatedAccessToken.js';
+import generatedRefreshToken from '../utils/generatedRefreshToken.js';
+
 
 
 export async function registerUserController(request, response) {
@@ -106,6 +108,108 @@ export async function verifyEmailController(request, response) {
             message: error.message || error,
             error: true,
             success: true
+        })
+    }
+}
+
+// login controller
+export async function loginUserController(req,res){
+    try{
+        const {email, password} = req.body ;
+
+        const user = await UserModel.findOne({email:email})
+    
+        if(!user){
+            return res.status(400).json({
+                message:"User not registered",
+                error:true,
+                success:false
+            })
+        }
+    
+        if(user.status!=="Active"){
+            return res.status(400).json({
+                message:"Contact to admin",
+                error:true,
+                success:false
+            })
+        }
+    
+        const checkPassword = await bcryptjs.compare(password, user.password);
+    
+        if(!checkPassword){
+            return res.status(400).json({
+                message:"Check your password",
+                error:true,
+                success:false
+            })
+        }
+    
+        
+        const accessToken = await generatedAccessToken(user._id);
+        const refreshToken = await generatedRefreshToken(user._id);
+    
+        const updateUser = await UserModel.findByIdAndUpdate(user?._id,{
+            last_login_date : new Date()
+        })
+    
+    
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
+        res.cookie('accessToken', accessToken,cookiesOption);
+        res.cookie('refreshToken', refreshToken,cookiesOption);
+    
+    
+        return res.json({
+            message : "Login successfully",
+            error : false,
+            success : true,
+            data :{
+                accessToken,
+                refreshToken
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false 
+        })
+    }
+}
+
+
+// logout controller 
+export async function logoutController(req, res){
+    try {
+        const userid = req.userId;  //middleware
+
+        const cookiesOption = {
+            httpOnly : true,
+            secure : true,
+            sameSite : "None"
+        }
+
+        res.clearCookie('accessToken', cookiesOption);
+        res.clearCookie('refreshToken', cookiesOption);
+
+        const removeRefreshToken = await UserModel.findByIdAndUpdate(userid,{
+            refresh_token : ""
+        })
+
+        return res.json({
+            message : "LoggedOut successfully",
+            error : false,
+            success : true
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message : error.message || error,
+            error : true,
+            success : false
         })
     }
 }
--- route/useroute.js
+++ route/useroute.js
@@ -1,9 +1,12 @@
 import {Router} from 'express'
-import {registerUserController,verifyEmailController} from '../controllers/usercontroller.js';
+import {registerUserController,verifyEmailController,loginUserController,logoutController} from '../controllers/usercontroller.js';
+import auth from '../middlewares/auth.js';
 
 
 const userRouter = Router()
 userRouter.post('/register', registerUserController)
 userRouter.post('/verifyEmail', verifyEmailController)
+userRouter.post('/login',loginUserController)
+userRouter.get('/logout', auth, logoutController)
 
 export default userRouter;
