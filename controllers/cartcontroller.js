684bf9d1bdb43cf08daac7a52d233e3e560b2f82
dev <107792454+devendrasinghpanwar@users.noreply.github.com>
2025-11-25 15:28:10 +0530

    this is last of commit video 34

--- controllers/cartcontroller.js
+++ controllers/cartcontroller.js
@@ -1,6 +1,7 @@
 import { request, response } from "express";
 import CartProductModel from "../models/cartproductmodel.js"; 
 import UserModel from "../models/usermodel.js";
+// import { useReducer } from "react";
 
 
 export const addToCartItemController= async (request,response)=>{
@@ -56,5 +57,124 @@ export const addToCartItemController= async (request,response)=>{
             message:error.message || error,
             error : true,
         })
+    }
+}
+
+export const getCartItemController = async (request,response)=>{
+    try{
+        const userId = request.userId;
+
+        const cartItem= await CartProductModel.find({
+            userId : userId
+        }).populate('productId')
+
+
+        return response.json({
+            data: cartItem,
+            error : false,
+            success : true
+        })
+        
+    } catch(error){
+        return response.status(500).json({
+            message:error.message || error,
+            error : true,
+            success : false
+        })
+    }
+}
+
+export const updateCartQtyController = async(request,response)=>{
+    try {
+        const userId = request.userId;
+        const {_id,qty}= request.body;
+
+        if(!_id || !qty){
+            return response.status(400).json({
+                message : "provide_id, qty"
+            })
+        }
+       
+        const updateCartitem = await CartProductModel.updateOne(
+            {
+            _id : Id,
+            userId : userId
+        },
+        {
+            quantity : qty
+        })
+        
+        return response.json({
+            message : "Update cart",
+            success : true,
+            error : false,
+            data : updateCartitem
+        })
+
+
+    } catch (error) {
+        return response.status(500).json({
+            message : error.message || error,
+            error : true,
+            success: false
+        })
+        
+    }
+}
+
+
+export const deleteCartItemQtyController = async ( request,response)=>{
+    try {
+        const userId = request.userId  // middleware
+        const {_id, productId} = request.body;
+
+
+        if(!_id){
+            return response.status(400).json({
+                message : "Provide _id",
+                error: true,
+                success : false
+            })
+        }
+
+        const deleteCartItem = await CartProductModel.deleteOne({_id : _id,userId : userId})
+
+        if(!deleteCartItem){
+            return response.status(900).json({
+                message: "The Product in the cart is not found",
+                error: true,
+                success: false
+            })
+        }
+
+        const user = await UserModel.findOne({
+            _id : userId
+        })
+
+
+        const cartItem = user?.shopping_cart;
+
+        const updatedUserCart = [...cartItem.slice(0,cartItem.indexOf(productId)),...cartItem.slice(cartItem.indexOf(productId)+1)];
+
+        user.shopping_cart = updatedUserCart;
+
+        await user.save();
+
+
+        return response.json({
+            message : "Item remove",
+            error : false,
+            success : true,
+            data : deleteCartItem
+        })
+
+        
+    } catch (error) {
+        return response.status(500).json({
+            message: error.message || error,
+            error : true,
+            success : false
+        })
+        
     }
 }
--- route/cartroute.js
+++ route/cartroute.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { addToCartItemController } from "../controllers/cartcontroller.js";
+import { addToCartItemController, deleteCartItemQtyController, getCartItemController, updateCartQtyController } from "../controllers/cartcontroller.js";
 import auth from "../middlewares/auth.js";
 
 const cartRouter = Router();
 
 cartRouter.post('/add', auth, addToCartItemController);
-
+cartRouter.get('/get', auth, getCartItemController);
+cartRouter.put('/update-qty',auth,updateCartQtyController);
+cartRouter.delete('/delete-cart-item',auth,deleteCartItemQtyController);
 export default cartRouter;
